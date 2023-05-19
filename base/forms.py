11f898ea24591ff7265cd08f6625acdda0d8cf27
Rupesh Ghimire <rupeshghimire007@gmail.com>
2023-05-19 17:47:35 +0545

    Made models, views, urls, templates and environment variables

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,8 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from .models import User,NoticeBoard,Question
+from django import forms
+
 
 
 class MyUserCreationForm(UserCreationForm):
@@ -14,3 +16,18 @@ class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['name', 'username', 'email', 'bio']
+
+
+
+class NoticeForm(forms.ModelForm):
+    class Meta:
+        model = NoticeBoard
+        fields = "__all__"
+
+
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = '__all__'
---
+++ base/templatetags/custom_tags.py
@@ -0,0 +1,30 @@
+from django import template
+import json
+import random
+register = template.Library()
+
+
+
+@register.filter(name='get_options')
+def get_options(value):
+    option = json.loads(value)
+    random.shuffle(option)
+    return option
+
+
+@register.filter(name='get_level')
+def get_level(value):
+    if value == 'E':
+        return "Marks:1"
+    elif value == 'M':
+        return "Marks:2"
+    elif value == 'H':
+        return "Marks:3"
+    else:
+        return 'Unknown'
+    
+
+@register.filter(name='get_subject')
+def get_subject(value):
+    if value==None or value=='':
+        return "Minor Project"
